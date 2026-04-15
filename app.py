e9c52245100ac6e123fff41f05ba5f852cf2f31a
Amaresha H <amareshambhi2006@gmail.com>
2026-04-15 23:50:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 API_KEY = os.environ.get("GEMINI_API_KEY")
 if not API_KEY:
     # ಒಂದು ವೇಳೆ Render ನಲ್ಲಿ ಸೆಟ್ ಮಾಡದಿದ್ದರೆ ಇಲ್ಲಿ ನೇರವಾಗಿ ಹಾಕಿಕೊಳ್ಳಿ
-    API_KEY = "AIzaSyChCaYwSLX9umtNUETkflkdNtpGoyKjNoA"
+    API_KEY = "AIzaSyA1_nysC8xkn589OZBH3NCGuz8hZJY7f18"
 
 genai.configure(api_key=API_KEY)
 
