6c6199a67cbb43646de82dcc0535a5782eae7770
Amaresha H <amareshambhi2006@gmail.com>
2026-04-15 23:09:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ API_KEY = os.environ.get("AIzaSyChCaYwSLX9umtNUETkflkdNtpGoyKjNoA")
 genai.configure(api_key=API_KEY)
 
 # ಮಾಡೆಲ್ ಸೆಟ್ ಮಾಡುವುದು
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
 # ಮೆಮೊರಿಗಾಗಿ ಗ್ಲೋಬಲ್ ಚಾಟ್ ಸೆಷನ್ ಆರಂಭಿಸುವುದು
 chat_session = model.start_chat(history=[])
