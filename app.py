7cee230a7d65189d7dbf176fcb9627b1091b1097
Amaresha H <amareshambhi2006@gmail.com>
2026-04-16 10:55:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if not API_KEY:
 
 genai.configure(api_key=API_KEY)
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-pro')
 
 @app.route('/')
 def home():
