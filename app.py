a719288d6f354e8157276d48470d734856df906b
Amaresha H <amareshambhi2006@gmail.com>
2026-04-16 12:29:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-# ಇಲ್ಲಿ ಹೊಸದಾಗಿ ಕಾಪಿ ಮಾಡಿದ API Key ಪೇಸ್ಟ್ ಮಾಡಿ
+
 API_KEY = os.environ.get("GEMINI_API_KEY")
 genai.configure(api_key=API_KEY)
 
-# ನಿಮ್ಮ ಸ್ಕ್ರೀನ್‌ನಲ್ಲಿ ತೋರಿಸುತ್ತಿರುವ ಮಾಡೆಲ್ ಹೆಸರು ಇಲ್ಲಿದೆ
 model = genai.GenerativeModel('gemini-3-flash-preview')
 
 @app.route('/')
